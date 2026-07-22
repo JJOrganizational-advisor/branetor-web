50e08d5c37e67ff82e36c64a5d00fbf4d550df58
Julio Jara <juliojaraacuna@gmail.com>
2026-07-22 16:00:59 -0700

    Quitar sección de ícono/imagen del hero

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,5 @@
 import Nav from "@/components/Nav";
 import Hero from "@/components/Hero";
-import ProductPreview from "@/components/ProductPreview";
 import HowItWorks from "@/components/HowItWorks";
 import Footer from "@/components/Footer";
 import { setRequestLocale } from "next-intl/server";
@@ -16,7 +15,6 @@ export default function HomePage({
     <main>
       <Nav />
       <Hero />
-      <ProductPreview />
       <HowItWorks />
       <Footer />
     </main>
