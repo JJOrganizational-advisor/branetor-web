d334039c3add2f9f2fab94be14446bb06e849d56
Julio Jara <juliojaraacuna@gmail.com>
2026-07-22 15:47:36 -0700

    Reemplazar placeholder de captura por ícono de marca simple

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,6 @@
 import Nav from "@/components/Nav";
 import Hero from "@/components/Hero";
+import ProductPreview from "@/components/ProductPreview";
 import HowItWorks from "@/components/HowItWorks";
 import Footer from "@/components/Footer";
 import { setRequestLocale } from "next-intl/server";
@@ -15,6 +16,7 @@ export default function HomePage({
     <main>
       <Nav />
       <Hero />
+      <ProductPreview />
       <HowItWorks />
       <Footer />
     </main>
---
+++ components/ProductPreview.tsx
@@ -0,0 +1,9 @@
+import BrandMark from "./BrandMark";
+
+export default function ProductPreview() {
+  return (
+    <section className="max-w-[1080px] mx-auto px-8 pb-4 flex justify-center">
+      <BrandMark />
+    </section>
+  );
+}
